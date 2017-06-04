64104bfbe46f9216414349c52773b386da84ff4d
tsingzdev <huiya.zhu@gmail.com>
2017-06-05 02:35:55 +0800

    update rising-temperature.sql

--- database/rising-temperature.sql
+++ database/rising-temperature.sql
@@ -45,7 +45,7 @@ SELECT * FROM Weather;
 /*SELECT Id, DATE_FORMAT(Date,'%Y%m%d'), Temperature FROM Weather; 
 */
 
-/* S1: Passed: add TO_DAYS */
+/* S1: use TO_DAYS */
 SELECT B.Id  FROM  Weather AS A , Weather AS B WHERE 
 TO_DAYS(B.Date) - TO_DAYS(A.Date) =1 AND B.Temperature > A.Temperature;
 
@@ -53,4 +53,16 @@ TO_DAYS(B.Date) - TO_DAYS(A.Date) =1 AND B.Temperature > A.Temperature;
 /* S2: Wrong Answer:  DATE_FORMAT instead of TO_DAYS, got error  */
 SELECT B.Id  FROM  Weather AS A , Weather AS B WHERE 
 DATE_FORMAT(B.Date,'%Y%m%d') - DATE_FORMAT(A.Date,'%Y%m%d') =1 AND B.Temperature > A.Temperature;
+
+
+/*S3: use DATEDIFF*/
+
+SELECT B.Id  FROM  Weather AS A , Weather AS B WHERE
+DATEDIFF(B.Date, A.Date) =1 AND B.Temperature > A.Temperature;
+
+
+/*S4: use ADDDATE, also look up to  DATE_ADD(), DATE_SUB() and so on. 
+reference: https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_adddate*/ 
+SELECT B.Id  FROM  Weather AS A , Weather AS B WHERE
+ADDDATE(A.Date, 1) = B.Date AND B.Temperature > A.Temperature;
 
