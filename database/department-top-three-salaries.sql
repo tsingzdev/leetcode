20093d528aa494cfc0fd89e745e93a8feadd8f5a
tsingzdev <huiya.zhu@gmail.com>
2017-06-05 23:38:16 +0800

    update department-top-three-salaries.sql

--- database/department-top-three-salaries.sql
+++ database/department-top-three-salaries.sql
@@ -33,4 +33,9 @@ Write a SQL query to find employees who earn the top three salaries in each of t
 +------------+----------+--------+
 
 */
+SELECT D.Name AS Department, E.Name AS Employee, E.Salary AS Salary 
+FROM Employee E INNER JOIN Department D ON E.DepartmentId = D.Id 
+WHERE (SELECT COUNT(DISTINCT(Salary)) FROM Employee 
+               WHERE DepartmentId = E.DepartmentId AND Salary > E.Salary) < 3
+ORDER by E.DepartmentId, E.Salary DESC;
 
