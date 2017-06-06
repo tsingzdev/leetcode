fcaeaf17747b92b56c5305ba143e8d7b523b5f2b
tsingzdev <huiya.zhu@gmail.com>
2017-06-06 17:47:56 +0800

    update transpose-file.sh

--- shell/transpose-file.sh
+++ shell/transpose-file.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #https://leetcode.com/problems/transpose-file/ 
 
 awk '
@@ -19,21 +18,3 @@ END {
 }' file.txt
 
 
-# Solution 2
-#awk '
-#{ 
-#    for(i=1; i<=NF; ++i) {
-#        if (word[i] == "") {
-#            word[i] = $i
-#        } else{
-#            word[i] = word[i]" "$i
-#        }
-#    }
-#} 
-#
-#END { 
-#    for(i=1; i<=NF; ++i) {
-#        print word[i]
-#    }
-#}' file.txt
-#
