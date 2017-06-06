8fe0be46615aa26bb5773314e2bc306b3d09947a
tsingzdev <huiya.zhu@gmail.com>
2017-06-06 17:40:18 +0800

    update word-frequency.sh

--- shell/word-frequency.sh
+++ shell/word-frequency.sh
@@ -2,10 +2,12 @@
 # https://leetcode.com/problems/word-frequency/
 
 # https://leetcode.com/discuss/29049/my-simple-solution-one-line-with-pipe
-#cat words.txt | tr -s ' ' '\n' | sort -r | uniq -c | awk '{print $2, $1}' # buggy
+
 #Solution 1:
+#cat words.txt | tr -s ' ' '\n' | sort -r | uniq -c | awk '{print $2, $1}' # buggy
 cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{print $2, $1}' 
 
-#Solution 2
-#cat words.txt | tr -s '[[:space:]]' '\n'| sort | uniq -c | sort -r | sed -r -e 's/[[:space:]]*([[:digit:]]+)[[:space:]]*([[:alpha:]]+)/\2 \1/g'
 
+#Solution2: 
+#awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' words.tx    t # not working, no sort
+awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' words.txt     | sort -k2 -nr
