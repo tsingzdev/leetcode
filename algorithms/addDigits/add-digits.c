10e4a5f6670733fe641bc9af3ed2de5c90cd6a85
tsingzdev <huiya.zhu@gmail.com>
2017-06-10 00:41:38 +0800

    update add-digits.c

--- algorithms/addDigits/add-digits.c
+++ algorithms/addDigits/add-digits.c
@@ -9,7 +9,8 @@ Given a non-negative integer num, repeatedly add all its digits until the result
  Follow up:
  Could you do it without any loop/recursion in O(1) runtime?
  * */
-#include<stdio.h>
+#include <stdio.h>
+#include <assert.h>
 
 
 int addDigits0(int num) {
@@ -26,10 +27,26 @@ int addDigits1(int num) {
             ret = ret/10+ret%10;
     }
     return ret;
+}
 
+int addDigits2(int num) {
+    while (num > 9) {
+        num = num/10 + num%10;
+    }
+    return num;
+}
+
+int addDigits(int num) {
+    return num > 9 ? ((num%9)==0 ? 9 : num%9  ) :num;
 }
 
 int main () {
+
+    assert(addDigits(0)==0);
+    assert(addDigits(1)==1);
+    assert(addDigits(18)==9);
+    assert(addDigits(12345)==6);
+    printf("all tests passed!\n");
 
     printf(" %d\n", addDigits(1235));
 
