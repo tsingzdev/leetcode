4cd7dbd9682c071dcb72b3e124a267589cca8814
tsingzdev <huiya.zhu@gmail.com>
2017-06-13 23:32:08 +0800

    add single-number-iii.py

---
+++ algorithms/singleNumberIII/single-number-iii.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/single-number-iii/#/description
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def singleNumber(self, nums):
+        x_xor_y = reduce(operator.xor, nums)
+        bit =  x_xor_y & -x_xor_y
+        result = [0, 0]
+        for i in nums:
+            result[bool(i & bit)] ^= i
+        return result
