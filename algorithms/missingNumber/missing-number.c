277234d302343c7c24d7ee1fb86037dc6b91f6f1
tsingzdev <huiya.zhu@gmail.com>
2017-07-08 23:46:55 +0800

    Add missingNumber solution

---
+++ algorithms/missingNumber/missing-number.c
@@ -0,0 +1,23 @@
+/*URL: https://leetcode.com/problems/missing-number/#/description
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ *
+ * For example,
+ * Given nums = [0, 1, 3] return 2.
+ *
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ *
+ * */
+
+int missingNumber(int* nums, int numsSize) {
+    int n = numsSize;
+    int ret = 0;
+    int sum = 0;
+    
+    for (int i = 0; i<n; i++) {
+        sum+=nums[i];        
+    }
+    ret = n*(n+1) /2 - sum;
+    return ret;
+        
+}
