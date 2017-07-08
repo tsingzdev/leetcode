5fb97020dec5b0171a002e2228c45e8435d30587
tsingzdev <huiya.zhu@gmail.com>
2017-07-08 23:28:32 +0800

    Update majority-element.c

--- algorithms/majorityElement/majority-element.c
+++ algorithms/majorityElement/majority-element.c
@@ -25,13 +25,39 @@ int majorityElement(int* nums, int numsSize) {
     return ret;
 }
 
+// solution 2
+int majorityElement2(int* nums, int numsSize) {
+    int major;
+    int i;
+
+    major = nums[0];
+    int count = 1;
+
+    for ( i = 1; i < numsSize; i++) {
+        if (nums[i] == major)
+            count++;
+        else count --;
+
+        if (count == 0) {
+            major = nums[i];
+            count = 1;
+        }
+    
+    }
+
+    return major;
+
+}
+
 int main(int argc, char** argv)
 {
     int array[] = {3,3,3,3,3,3,1,2,1,2,1};
     int array2[] = {8,8,7,7,7};
 
     printf("%d\n", majorityElement(array, 11));
+    printf("%d\n", majorityElement2(array, 11));
     printf("%d\n", majorityElement(array2, 5));
+    printf("%d\n", majorityElement2(array2, 5));
 
     return 0;
 }
