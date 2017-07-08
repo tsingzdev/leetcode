527b43f09a6669e5ce538195862f77762770728b
tsingzdev <huiya.zhu@gmail.com>
2017-07-08 22:49:31 +0800

    Add majorityElement solution

---
+++ algorithms/majorityElement/majority-element.c
@@ -0,0 +1,37 @@
+/* URL: https://leetcode.com/problems/majority-element/#/description
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//reference: http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
+int majorityElement(int* nums, int numsSize) {
+    int ret ;
+    int cnt = 0 ;
+
+    for (int i = 0; i< numsSize; i++) {
+        if (cnt ==0) {
+            ret = nums[i];
+            cnt ++;
+        } else {
+            ret == nums[i] ? cnt++ : cnt --; 
+            //if (cnt >= numsSize/2) 
+            //    return ret;
+        }
+    } 
+    return ret;
+}
+
+int main(int argc, char** argv)
+{
+    int array[] = {3,3,3,3,3,3,1,2,1,2,1};
+    int array2[] = {8,8,7,7,7};
+
+    printf("%d\n", majorityElement(array, 11));
+    printf("%d\n", majorityElement(array2, 5));
+
+    return 0;
+}
