fe425f7f59b8a628a564a0fb09d83b33f29258a8
tsingzdev <huiya.zhu@gmail.com>
2017-07-11 18:26:41 +0800

    Update bitwise-and-of-numbers-range.c

--- algorithms/bitwiseANDofNumbersRange/bitwise-and-of-numbers-range.c
+++ algorithms/bitwiseANDofNumbersRange/bitwise-and-of-numbers-range.c
@@ -7,7 +7,9 @@
  * */
 #include <stdio.h>
 
-//The solution is Time Limit Exceeded .   
+// Solution 1:  is Time Limit Exceeded .   
+// see to this : http://bookshadow.com/weblog/2015/04/17/leetcode-bitwise-and-numbers-range/
+// given the range 0 <= m <= n <= 2147483647, time complexity more than O(n) is not acceptable.
 int rangeBitwiseAnd_0(int m, int n) {
     int ret= m;
     while (m<n) {
@@ -17,6 +19,7 @@ int rangeBitwiseAnd_0(int m, int n) {
     return ret;
 }
 
+// Solution 2
 int rangeBitwiseAnd(int m, int n) {
     while (m < n) {
         n = n & n-1;
@@ -24,9 +27,21 @@ int rangeBitwiseAnd(int m, int n) {
     return m&n;
 }
 
+// Solution 3 
+// the result should be the common left header of m and n
+int rangeBitwiseAnd(int m, int n) {
+    int p = 0;
+    int ret = 0;
+    while (m!=n){
+        m>>=1;
+        n>>=1;
+        p+=1;
+    }
+    ret = m<<p;
+    return ret;
+}
 
 int main () {
     int m = 5, n = 7;
     printf("%d\n", rangeBitwiseAnd(m,n));
-
 }
