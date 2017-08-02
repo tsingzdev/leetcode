493c14c21ac1ea7423ef6dbb42136b050d7fd8f2
tsingzdev <huiya.zhu@gmail.com>
2017-08-02 14:57:43 +0800

    Add longestSubstringWithoutRepeatingCharacters c++ solution

---
+++ algorithms/longestSubstringWithoutRepeatingCharacters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,53 @@
+/*URL:https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ *
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ *
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ *
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ * */
+#include<stdio.h>
+#include<string.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    const int MAX_CHARS = 256;
+    int m[MAX_CHARS];
+    memset(m ,-1, sizeof(m));
+
+    int maxLen = 0;
+    int lastRepeatPos = -1;
+    for( int i =0; i<s.size(); i++) {
+        if(m[s[i]]!= -1&& lastRepeatPos < m[s[i]]) {
+            lastRepeatPos = m[s[i]];
+        }
+        if(i - lastRepeatPos > maxLen) {
+            maxLen = i - lastRepeatPos;
+        }
+        m[s[i]] = i;
+    }
+    return maxLen;
+} 
+int main ( int argc, char**argv) {
+    string s = "abcabcbb";
+    cout << s << ":" << lengthOfLongestSubstring(s) << endl;
+    
+    s = "bbbbb";
+    cout << s << ":" << lengthOfLongestSubstring(s) << endl;
+
+    s = "bbabcdb";
+    cout << s << ":" << lengthOfLongestSubstring(s) << endl;
+
+    if (argc >1) {
+        s = argv[1];
+        cout << s << ":" << lengthOfLongestSubstring(s) << endl;
+    }
+    return 0;
+}
+
