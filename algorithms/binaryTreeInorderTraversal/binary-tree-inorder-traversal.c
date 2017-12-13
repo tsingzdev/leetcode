f883b6267e5b1fc1ccfbe6792d0a4903cdaf0675
tsingzdev <huiya.zhu@gmail.com>
2017-12-13 23:26:43 +0800

    Add binaryTreeInorderTraversal solutions

---
+++ algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.c
@@ -0,0 +1,34 @@
+/*URL:https://leetcode.com/problems/binary-tree-inorder-traversal/#/description
+ *Given a binary tree, return the inorder traversal of its nodes' values.
+
+ *For example:
+ *Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+ * return [1,3,2].
+
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *
+ * */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    
+
+
+    
+}
+
---
+++ algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+		
+        
+    }
+}
