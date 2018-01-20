1a6e5300e5be7e23cc2793887b28a02631950452
tsingzdev <huiya.zhu@gmail.com>
2018-01-20 19:54:15 +0800

    update powx-n.c solution

--- algorithms/powxN/powx-n.c
+++ algorithms/powxN/powx-n.c
@@ -2,7 +2,7 @@
 /*Implement pow(x, n).
 
 
-Example 1:
+  Example 1:
 
 Input: 2.00000, 10
 Output: 1024.00000
@@ -15,8 +15,8 @@ Output: 9.26100
 #include <stdio.h>
 
 // Solution 0:
-// Time Limit Exceeded !!
-double 0_myPow(double x, int n) {
+// 1. Time Limit Exceeded caused by the while loop; 2. Also neglect the case when n < 0.
+double myPow_0(double x, int n) {
     double ret = 1.00;    
     while (n ) {
         ret = ret*x;
@@ -25,11 +25,15 @@ double 0_myPow(double x, int n) {
     return ret;
 }
 
-//Solution 1:
+//Solution 1: accepted.
 double myPow(double x, int n) {
-        
-}
+    if (n ==0) return 1;     
+    if (n < 0) return 1/x * myPow(1/x, -(n+1));
+    if (n==2) return x*x;
 
+    if (n%2==0) return myPow( myPow(x, n/2), 2);
+    else return x*myPow( myPow(x, n/2), 2);
+}
 
 
 int main () {
