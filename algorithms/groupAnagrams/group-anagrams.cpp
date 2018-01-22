08d2090b2090b39f804f22a10fae0b3c97cdb47d
tsingzdev <huiya.zhu@gmail.com>
2018-01-22 17:13:38 +0800

    update group-anagrams.cpp

--- algorithms/groupAnagrams/group-anagrams.cpp
+++ algorithms/groupAnagrams/group-anagrams.cpp
@@ -48,18 +48,3 @@ public:
     }
 };
 
-/*
-int main() {
-    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    Solutions s;
-    auto ans = s.groupAnagrams(strs);
-
-    for(int i = 0; i < ans.size(); i++) {
-        for (int j = 0; j< ans[i].size(), j++) {
-            cout << ans[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-*/
