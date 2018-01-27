905af1a2853e27088ef2edfe67904829bafd89c4
tsingzdev <huiya.zhu@gmail.com>
2018-01-27 23:59:46 +0800

    add rotate-image.cpp

--- algorithms/rotateImage/rotate-image.cpp
+++ algorithms/rotateImage/rotate-image.cpp
@@ -40,9 +40,19 @@ rotate the input matrix in-place such that it becomes:
 
  * */ 
 
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-                        
+        const int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - 1 - i; ++j) {
+                const auto tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1- j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
+            }
+        }
     }
 };
