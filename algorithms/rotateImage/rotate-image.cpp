d609dc32fad029d63c2005dc25bcb876b46d155a
tsingzdev <huiya.zhu@gmail.com>
2018-01-28 23:30:30 +0800

    update rotate-image.cpp

--- algorithms/rotateImage/rotate-image.cpp
+++ algorithms/rotateImage/rotate-image.cpp
@@ -56,3 +56,22 @@ public:
         }
     }
 };
+
+class Solution {
+public: 
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        // anti-diagonal mirror
+        for (int i =0; i< n; ++i) {
+            for (int j= 0; j< n-i; ++j) {
+                swap(matrix[i][j], matrix[n-1-j][n-1-i]);
+            }
+        }
+        // horizontal mirror
+        for (int i = 0; i<n/2; ++i) {
+            for(int j =0; j<n; ++j) {
+                swap(matrix[i][j], matrix[n-1-i][j]);
+            } 
+        }
+    }
+};
