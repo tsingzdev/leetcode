bb1588dda84f83a36290710947783854d44a89dd
tsingzdev <huiya.zhu@gmail.com>
2018-01-30 17:11:07 +0800

    Add permutations solution

---
+++ algorithms/permutations/permutations.py
@@ -0,0 +1,28 @@
+
+class Solution(object):
+    def permute(self, num):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        result = []
+        used = [False] * len(num)
+        self.permuteRecu(result, used, [], num)
+        return result
+
+    def permuteRecu(self, result, used, cur, num):
+        if len(cur) == len(num):
+            result.append(cur + [])
+            return
+
+        for i in xrange(len(num)):
+            if not used[i]:
+                used[i] = True
+                cur.append(num[i])
+                self.permuteRecu(result, used, cur, num)
+                cur.pop()
+                used[i] = False
+
+if __name__ == "__main__":
+    print Solution().permute([1,2,3])
