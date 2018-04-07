3cd64c2f0e75dfbedfdd102bfcc47dc6d1ec40e2
tsingzdev <huiya.zhu@gmail.com>
2018-04-07 20:31:00 +0800

    Update unique-binary-search-trees.py

--- algorithms/uniqueBinarySearchTrees/unique-binary-search-trees.py
+++ algorithms/uniqueBinarySearchTrees/unique-binary-search-trees.py
@@ -22,10 +22,22 @@ class Solution(object):
             count =1
             # C (n,k) = (n)/1*(n-1)/2 ... *(n-k+1)/k
             for i in xrange(1, k+1):
-                count = count+ (n-i+1)/i
+                count = count* (n-i+1)/i
             return count
         return combination(2*n, n) - cimbination(2*n, n-1)
 
+class Solution2(object):
+    def numTress(self, n):
+        counts = [1,1]
+        for i in xrange(2, n+1):
+            count = 0
+            for j in xrange(i):
+                count+= counts[j]*counts[i-j-1]
+            counts.append(count)
+        return count[-1]
+
+
+            
 
 
          
