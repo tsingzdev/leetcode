26c3b3d9023096ed25be5c845f0c60d2702c17b2
tsingzdev <huiya.zhu@gmail.com>
2018-04-11 22:33:24 +0800

    Updated unique-binary-search-trees.c

--- algorithms/uniqueBinarySearchTrees/unique-binary-search-trees.c
+++ algorithms/uniqueBinarySearchTrees/unique-binary-search-trees.c
@@ -44,3 +44,21 @@ int numTrees(int n) {
     }
     return count[n];
 }
+
+// Solution 3: Accepted
+int numTrees(int n) {
+    if(n<3) return n;
+    int *d = (int*) calloc(n+1, sizeof(int));
+    int i, j;
+    d[0] = 1; /*dummy*/
+    d[1] = 1;
+    d[2] = 2;
+
+    for(i = 3; i<=n;i++){
+        for(j=0; j<i; j++) {
+            d[i]+=d[j]*d[i-1-j];
+        }
+    }
+    return d[n];
+}
+
