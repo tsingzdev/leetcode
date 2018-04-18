126dc873d6679958b35469e01a40a302d5f4cdcd
tsingzdev <huiya.zhu@gmail.com>
2018-04-18 23:03:50 +0800

    Update binary-tree-inorder-traversal.py, add solution2 using stack

--- algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.py
+++ algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.py
@@ -28,4 +28,23 @@ class Solution:
                     node.right = None
                     curr = curr.right
         return result
-        
+
+class Solution2(object):        
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype List[int]
+        """
+        result, stack = [], [(root, False)]
+        while stack:
+            root, is_visited = stack.pop()
+            if root is None:
+                continue
+            if is_visited:
+                result.append(root.val)
+            else:
+                stack.append((root.right, False))
+                stack.append((root, True))
+                stack.append((root.left, False))
+        return result
+
