a8016e01e732823a2894ca440361a8f9a981d12c
tsingzdev <huiya.zhu@gmail.com>
2018-05-17 23:44:05 +0800

    Add majority-element.cpp

---
+++ algorithms/majorityElement/majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = nums[0], cnt = 1;
+        for (const auto& i : nums) {
+            if (i == ans) {
+                ++cnt;
+            } else {
+                --cnt;
+                if (cnt == 0) {
+                    ans = i;
+                    cnt = 1;
+                }
+            }
+        }
+        return ans; 
+    }
+};
