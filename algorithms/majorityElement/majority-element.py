d7830496221cc33c2034e33a36b7152b0dbbb8eb
tsingzdev <huiya.zhu@gmail.com>
2018-05-18 23:52:00 +0800

    Add majority-element.py

---
+++ algorithms/majorityElement/majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sorted(collections.Counter(nums).items(), key=lambda a: a[1], reverse=True)[0][0]
+
+if __name__ == "__main__":
+    print Solution().majorityElement([1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6])
