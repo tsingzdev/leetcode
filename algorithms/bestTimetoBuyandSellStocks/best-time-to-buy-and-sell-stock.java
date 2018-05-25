a6a039c65972c0bd4164db07d8f3aee8dcae0889
tsingzdev <huiya.zhu@gmail.com>
2018-05-26 01:39:53 +0800

    Update best-time-to-buy-and-sell-stock.java

--- algorithms/bestTimetoBuyandSellStocks/best-time-to-buy-and-sell-stock.java
+++ algorithms/bestTimetoBuyandSellStocks/best-time-to-buy-and-sell-stock.java
@@ -33,3 +33,16 @@ class Solution {
     }
 }
 
+//Solution2: 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        int max = 0, min = Integer.MAX_VALUE;       
+
+        for(int i=0; i<len; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i]-min);
+        }
+        return max;
+    }
+}
