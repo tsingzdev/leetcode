14750f4f9739cffe8db4f9285fa1814259e7c174
tsingzdev <huiya.zhu@gmail.com>
2018-05-25 16:20:13 +0800

    Update add-digits.py

--- algorithms/addDigits/add-digits.py
+++ algorithms/addDigits/add-digits.py
@@ -1,5 +1,16 @@
 #url: https://leetcode.com/problems/add-digits/#/description
+
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 # 
+# Example:
+# 
+# Input: 38
+# Output: 2
+# Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+#              Since 2 has only one digit, return it.
+# Follow up:
+# Could you do it without any loop/recursion in O(1) runtime?
+
 
 class Solution:
     def addDigits(self, num):
@@ -8,7 +19,7 @@ class Solution:
 
 if __name__ == '__main__':
     s= Solution()
-    r = s.addDigits(123456)
-    print r
+    result = s.addDigits(12345)
+    print result
 
 
