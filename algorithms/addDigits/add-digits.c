190cc9b9830f1f170d8d4202a205fea5545ec61d
tsingzdev <huiya.zhu@gmail.com>
2018-05-26 08:02:14 +0800

    Update add-digits.c/java

--- algorithms/addDigits/add-digits.c
+++ algorithms/addDigits/add-digits.c
@@ -1,20 +1,20 @@
 /*URL: https://leetcode.com/problems/add-digits/#/description
 
-Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-
- For example:
-
- Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
-
- Follow up:
- Could you do it without any loop/recursion in O(1) runtime?
+ *Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ * For example:
+ *
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
  * */
+
 #include <stdio.h>
 #include <assert.h>
 
-
 int addDigits0(int num) {
-        return 1+ (num-1)%9;
+    return 1+ (num-1)%9;
 }
 
 int addDigits1(int num) {
@@ -48,7 +48,9 @@ int main () {
     assert(addDigits(12345)==6);
     printf("all tests passed!\n");
 
-    printf(" %d\n", addDigits(1235));
+    printf(" %d\n", addDigits(18));
+    printf(" %d\n", addDigits(1));
+    printf(" %d\n", addDigits(12345));
 
     return 0;
 }
