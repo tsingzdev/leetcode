8c2215bca79221da2101618ed5b173b83324a3c2
tsingzdev <huiya.zhu@gmail.com>
2018-05-27 16:01:22 +0800

    Update binary-tree-inorder-traversal.java

--- algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.java
+++ algorithms/binaryTreeInorderTraversal/binary-tree-inorder-traversal.java
@@ -1,4 +1,16 @@
 // https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+/* Given a binary tree, return the inorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree [1,null,2,3],
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * return [1,3,2].
+ *
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
 
 /**
  * Definition for a binary tree node.
@@ -12,7 +24,20 @@
 
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-		
+        List<Integer> List = new ArrayList<Integer>();
         
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while(cur != null || !stack.empty()) {
+            while(cur != null) {
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        } 
+        return list;
     }
 }
