88fb61d9744d2bc961cde1daa7c2225f2912ad54
tsingzdev <huiya.zhu@gmail.com>
2018-05-29 16:03:17 +0800

    Update convertaNumbertoHexadecimal solutions

--- algorithms/convertaNumbertoHexadecimal/convert-a-number-to-hexadecimal.c
+++ algorithms/convertaNumbertoHexadecimal/convert-a-number-to-hexadecimal.c
@@ -1,26 +1,34 @@
 /*URL: https://leetcode.com/problems/convert-a-number-to-hexadecimal/#/description
- *Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, twoâ€™s complement method is used.
-
-Note:
-
-All letters in hexadecimal (a-f) must be in lowercase.
-The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
-The given number is guaranteed to fit within the range of a 32-bit signed integer.
-You must not use any method provided by the library which converts/formats the number to hex directly.
-Example 1:
-
-Input:
-26
-
-Output:
-"1a"
-Example 2:
-
-Input:
--1
-
-Output:
-"ffffffff"
+ *Given an integer, write an algorithm to convert it to hexadecimal. 
+ * For negative integer, two¡¯s complement method is used.
+ * 
+ * Note:
+ *
+ * 1. All letters in hexadecimal (a-f) must be in lowercase.
+ *
+ * 2. The hexadecimal string must not contain extra leading 0s. If the number is 
+ * zero, it is represented by a single zero character '0'; otherwise, the first 
+ * character in the hexadecimal string will not be the zero character.
+ *
+ * 3. The given number is guaranteed to fit within the range of a 32-bit signed integer.
+ *
+ * 4. You must not use any method provided by the library which converts/formats the number to hex directly.
+ *
+ * Example 1:
+ * 
+ * Input:
+ * 26
+ * 
+ * Output:
+ * "1a"
+ *
+ * Example 2:
+ * 
+ * Input:
+ * -1
+ * 
+ * Output:
+ * "ffffffff"
  * */
 //Solution 1:
 #define HEX_MAP(x)  ((x)["0123456789abcdef"])
@@ -59,7 +67,3 @@ char *toHex (int num) {
     }
     return ans;
 }
-
-
-
-
