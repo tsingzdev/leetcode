0f40f079c656629ff387bd6bb18947a0d9bc57a6
tsingzdev <huiya.zhu@gmail.com>
2018-06-03 18:33:12 +0800

    Update hamming-distance.c

--- algorithms/hammingDistance/hamming-distance.c
+++ algorithms/hammingDistance/hamming-distance.c
@@ -1,33 +1,30 @@
 /*https://leetcode.com/problems/hamming-distance/#/description
- The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
-
- Given two integers x and y, calculate the Hamming distance.
-
-Note:
-0 ≤ x, y < 231.
-
-Example:
-
-Input: x = 1, y = 4
-
-Output: 2
-
-Explanation:
-1   (0 0 0 1)
-4   (0 1 0 0)
-       ↑   ↑
-
-       The above arrows point to positions where the corresponding bits are different.
+ *  The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ * 
+ *  Given two integers x and y, calculate the Hamming distance.
+ * 
+ * Note:
+ * 0 ≤ x, y < 231.
+ * 
+ * Example:
+ * 
+ * Input: x = 1, y = 4
+ * 
+ * Output: 2
+ * 
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *        ↑   ↑
+ * 
+ * The above arrows point to positions where the corresponding bits are different.
  */
 #include <stdio.h>
-
-int numberof1Bits(int n);
 
 int hammingDistance(int x, int y) {
     int hdis = 0;
     int tmp = 0;
     tmp = x^y;
-	//hdis = numberof1Bits(tmp); 
     while(tmp) {
         tmp = tmp&(tmp-1);
         hdis++;
@@ -35,21 +32,8 @@ int hammingDistance(int x, int y) {
     return hdis; 
 }
 
-
-int numberof1Bits(int n) {
-    int ans = 0;
-    while (n) {
-        n = n & (n-1);
-        ans++;
-    }
-    return ans;
-}
-
 int main( ) {
 	int x = 1;
 	int y = 4;
 	printf("hamming distance of x and y is %d\n", hammingDistance(1,4));
-
 }
-
-
