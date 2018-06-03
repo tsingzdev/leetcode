b0951707c5aa530b144e30c7c67d97c4a93ec251
tsingzdev <huiya.zhu@gmail.com>
2018-06-03 16:01:54 +0800

    Update firstMissingPositive solutions

--- algorithms/firstMissingPositive/first-missing-positive.c
+++ algorithms/firstMissingPositive/first-missing-positive.c
@@ -1,17 +1,20 @@
-/*
+/* https://leetcode.com/problems/first-missing-positive/description/
  * Given an unsorted integer array, find the first missing positive integer.
 
- For example,
- Given [1,2,0] return 3,
- and [3,4,-1,1] return 2.
-
- Your algorithm should run in O(n) time and uses constant space.
+* For example,
+* Given [1,2,0] return 3,
+* and [3,4,-1,1] return 2.
+*
+* Your algorithm should run in O(n) time and uses constant space.
 
 */
 
-//Solution 1
+#include<stdio.h>
+#include<assert.h>
+
+/*Solution 1 */
 int firstMissingPositive(int* nums, int numsSize) {
-    int i;
+    int i;  
     for (i = 0; i< numsSize; i++) {
         while(nums[i] > 0 && nums[i]< numsSize && nums[nums[i]-1] != nums[i]) {
             int t = nums[i];
@@ -30,11 +33,18 @@ int firstMissingPositive(int* nums, int numsSize) {
     return i+1;
 }
 
-// Solution 2
+
+/*Solution 2 */
 void swap(int *a, int *b) {
     *a = *b-*a;
     *b = *b-*a;
     *a = *b+*a;
+}
+
+void swap_2(int a, int b) {
+    a = b-a;
+    b = b-a;
+    a = b+a;
 }
 
 int firstMissingPositive_2(int* nums, int numsSize) {
@@ -44,6 +54,7 @@ int firstMissingPositive_2(int* nums, int numsSize) {
             i++;
         else
             swap(&nums[i], &nums[nums[i]-1]);
+            //swap_2(nums[i], nums[nums[i]-1]);
     }
     for (i = 0; i < numsSize; i++)  {
         if(nums[i]!=i+1)
@@ -52,4 +63,25 @@ int firstMissingPositive_2(int* nums, int numsSize) {
 
     return i+1;
 
+}
+
+
+
+int main() {
+    int nums0[] = {1, 2, 0};
+    int nums1[] = {3, 4, -1, 1};
+    int nums2[] = {-1, 4, 2, 1, 9, 10};
+    int nums3[] = {1, 1};
+
+    int p1 = firstMissingPositive_2(nums0, 3);
+    int p2 = firstMissingPositive_2(nums1, 4);
+    int p3 = firstMissingPositive_2(nums2, 6);
+    int p4 = firstMissingPositive_2(nums3, 2);
+
+    printf("%d\n", p1);
+    printf("%d\n", p2);
+    printf("%d\n", p3);
+    printf("%d\n", p4);
+
+    return 0;
 }
--- algorithms/firstMissingPositive/first-missing-positive.py
+++ algorithms/firstMissingPositive/first-missing-positive.py
@@ -1,4 +1,21 @@
+#  https://leetcode.com/problems/first-missing-positive/description/
+# Given an unsorted integer array, find the smallest missing positive integer.
 # 
+# Example 1:
+# 
+# Input: [1,2,0]
+# Output: 3
+# Example 2:
+# 
+# Input: [3,4,-1,1]
+# Output: 2
+# Example 3:
+# 
+# Input: [7,8,9,11,12]
+# Output: 1
+# Note:
+# 
+# Your algorithm should run in O(n) time and uses constant extra space.
 
 class Solution(object):
     def firstMissingPositive(self, A):
@@ -20,3 +37,4 @@ class Solution(object):
 
 if __name__ == "__main__":
     print Solution().firstMissingPositive([1,2,0])
+    print Solution().firstMissingPositive([3, 4, -1, 1])
