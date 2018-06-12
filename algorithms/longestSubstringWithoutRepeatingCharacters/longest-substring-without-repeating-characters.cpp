713f2d6f9fe27b1d3c3cefd0e4dca661b078086e
tsingzdev <huiya.zhu@gmail.com>
2018-06-12 12:31:34 +0800

    Update longest-substring-without-repeating-characters.cpp: add solution using map

--- algorithms/longestSubstringWithoutRepeatingCharacters/longest-substring-without-repeating-characters.cpp
+++ algorithms/longestSubstringWithoutRepeatingCharacters/longest-substring-without-repeating-characters.cpp
@@ -10,12 +10,14 @@
  * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  *
  * */
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
+//Solution 1:
 int lengthOfLongestSubstring(string s) {
     const int MAX_CHARS = 256;
     int m[MAX_CHARS];
@@ -34,14 +36,33 @@ int lengthOfLongestSubstring(string s) {
     }
     return maxLen;
 } 
+
+//Solution 2: using a map 
+int lengthOfLongestSubstring1(string s) {
+    map<char, int> m;
+    int maxLen = 0;
+    int lastRepeatPos = -1;
+
+    for(int i=0; i<s.size(); i++) {
+        if(m.find(s[i])!= m.end() && lastRepeatPos <= m[s[i]]) {
+            lastRepeatPos = m[s[i]];
+        }
+        if(i - lastRepeatPos > maxLen) {
+            maxLen = i - lastRepeatPos;
+        }
+        m[s[i]] = i;
+    }
+    return maxLen;
+}
+
 int main ( int argc, char**argv) {
     string s = "abcabcbb";
     cout << s << ":" << lengthOfLongestSubstring(s) << endl;
-    
-    s = "bbbbb";
+
+    s = "pwwkew";
     cout << s << ":" << lengthOfLongestSubstring(s) << endl;
 
-    s = "bbabcdb";
+    s = "bbbbb";
     cout << s << ":" << lengthOfLongestSubstring(s) << endl;
 
     if (argc >1) {
