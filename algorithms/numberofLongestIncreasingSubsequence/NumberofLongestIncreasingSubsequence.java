ee923eb1731092d66a4f8e3e8f6b9fe1b35ddfc0
tsingzdev <huiya.zhu@gmail.com>
2018-06-16 12:22:22 +0800

    Update NumberofLongestIncreasingSubsequence.java

--- algorithms/numberofLongestIncreasingSubsequence/NumberofLongestIncreasingSubsequence.java
+++ algorithms/numberofLongestIncreasingSubsequence/NumberofLongestIncreasingSubsequence.java
@@ -1,7 +1,7 @@
 //https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
 /*Given an unsorted array of integers, find the number of longest increasing subsequence.
 
-Example 1:
+  Example 1:
 Input: [1,3,5,4,7]
 Output: 2
 Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
@@ -13,10 +13,45 @@ Note: Length of the given array will be not exceed 2000 and the answer is guaran
 */
 
 class NumberofLongestIncreasingSubsequence {
+    public static void main(String[] args) throws Exception {
+        int[] A = {1,3,5,4,7};
+        System.out.println(new NumberofLongestIncreasingSubsequence().findNumberOfLIS(A));
+    }
 
     public int findNumberOfLIS(int[] nums) {
-        
-
-
+        if(nums.length == 0) return 0;
+        int[] length = new int[nums.length];
+        length[0] = 1;
+        int maxVal = 1;
+        for(int i = 1; i < nums.length; i ++){
+            int max = 1;
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    max = Math.max(max, length[j] + 1);
+                    maxVal = Math.max(maxVal, max);
+                }
+            }
+            length[i] = max;
+        }
+        int[] count = new int[nums.length];
+        count[0] = 1;
+        for(int i = 1; i < length.length; i ++){
+            for(int j = 0; j < i; j++){
+                if((length[j] + 1 == length[i]) && (nums[j] < nums[i])){
+                    count[i] += count[j];
+                }
+            }
+            if(count[i] == 0){
+                count[i] = 1; //default is just 1
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < length.length; i ++){
+            if(length[i] == maxVal){
+                ans += count[i];
+            }
+        }
+        return ans;
     }
 }
+
