d176b0dcc98e27ed69a806d961a2d287d824be19
tsingzdev <huiya.zhu@gmail.com>
2018-06-29 20:07:04 +0800

    Update longest-increasing-subsequence.cpp

--- algorithms/longestIncreasingSubsequence/longest-increasing-subsequence.cpp
+++ algorithms/longestIncreasingSubsequence/longest-increasing-subsequence.cpp
@@ -13,6 +13,16 @@
  * Follow up: Could you improve it to O(n log n) time complexity?
  * */
 
-int lengthOfLIS(int* nums, int numsSize) {
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++) {
+            auto i=std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it ==res.end()) res.push_back(nums[i]);
+            else *it = nums[i]; 
+        }
+        return res.size(); 
+    }
+};
 
-}
