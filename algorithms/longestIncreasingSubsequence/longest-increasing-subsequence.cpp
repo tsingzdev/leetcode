9c48dc542a4d02312f872d4a02ac782efa33d5f4
tsingzdev <huiya.zhu@gmail.com>
2018-06-29 20:10:00 +0800

    Update longest-increasing-subsequence.cpp

--- algorithms/longestIncreasingSubsequence/longest-increasing-subsequence.cpp
+++ algorithms/longestIncreasingSubsequence/longest-increasing-subsequence.cpp
@@ -18,7 +18,7 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> res;
         for(int i=0; i<nums.size(); i++) {
-            auto i=std::lower_bound(res.begin(), res.end(), nums[i]);
+            auto it=std::lower_bound(res.begin(), res.end(), nums[i]);
             if(it ==res.end()) res.push_back(nums[i]);
             else *it = nums[i]; 
         }
