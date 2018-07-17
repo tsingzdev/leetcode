8a923d1bafb1c08dcb7e0b9cbba24f4ff0016b8d
tsingzdev <huiya.zhu@gmail.com>
2018-07-17 15:10:18 +0800

    Update palindrome-number.c: add sulution2

--- algorithms/palindromeNumber/palindrome-number.c
+++ algorithms/palindromeNumber/palindrome-number.c
@@ -10,20 +10,43 @@ bool isPalindrome(int x) {
     if (x == 0) {
         return true;
     }
-    
+
     int reversed = 0;
     int tmp = x;
     while (tmp > 0) {
         reversed = reversed *10 + tmp%10;
         tmp/=10;
     }
-    
+
     if (reversed > INT_MAX || reversed < INT_MIN )  
         return false;
-    
+
     if (reversed == x) {
         return true;
     } else {
         return false;
     }
 }
+
+//Solution2: 
+bool isPalindrome(int x) {
+    if(x < 0) {
+        return false;
+    }
+
+    int divisor = 1;
+    while (x / divisor >= 10) {
+        divisor *= 10;
+    }
+
+    for (; x > 0; x = (x % divisor) / 10, divisor /= 100) {
+        int left = x / divisor;
+        int right = x % 10;
+        if (left != right) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
